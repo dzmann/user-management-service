451878d8b1a25cc8e5814074e72b17fe260b420e
dzmann <dnzimermann@atsistemas.uy>
2021-12-15 10:00:10 -0300

    login issue fixed and added missing configuration for keycloak

--- src/main/java/com/example/usermanagementservice/UserManagementServiceApplication.java
+++ src/main/java/com/example/usermanagementservice/UserManagementServiceApplication.java
@@ -3,7 +3,6 @@ package com.example.usermanagementservice;
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication
 @EnableEncryptableProperties
--- src/main/java/com/example/usermanagementservice/controller/UserController.java
+++ src/main/java/com/example/usermanagementservice/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.example.usermanagementservice.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/v1/users")
 public class UserController {
 
-
+    @GetMapping
+    public String testController() {
+        return "accediste";
+    }
 
 }
