51dd731dfe003e950f414af79061e18e419be0f8
dzmann <dnzimermann@atsistemas.uy>
2021-12-16 11:45:55 -0300

    added property to ignore null fields

--- src/main/java/com/example/usermanagementservice/dto/UserDto.java
+++ src/main/java/com/example/usermanagementservice/dto/UserDto.java
@@ -1,5 +1,6 @@
 package com.example.usermanagementservice.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -8,6 +9,7 @@ import javax.validation.constraints.NotNull;
 
 @NoArgsConstructor
 @Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class UserDto {
 
     @NotNull
