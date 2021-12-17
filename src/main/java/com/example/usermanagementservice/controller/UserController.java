904e210b7a0a2ca6bffdc7684f24a188ddc2f1d9
dzmann <dnzimermann@atsistemas.uy>
2021-12-17 11:27:07 -0300

    fixed validated method response

--- src/main/java/com/example/usermanagementservice/controller/UserController.java
+++ src/main/java/com/example/usermanagementservice/controller/UserController.java
@@ -9,7 +9,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.security.RolesAllowed;
 import javax.validation.Valid;
--- src/main/java/com/example/usermanagementservice/exception/UserManagementExceptionHandler.java
+++ src/main/java/com/example/usermanagementservice/exception/UserManagementExceptionHandler.java
@@ -2,11 +2,19 @@ package com.example.usermanagementservice.exception;
 
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Order(Ordered.HIGHEST_PRECEDENCE)
 @ControllerAdvice
@@ -17,6 +25,17 @@ public class UserManagementExceptionHandler extends ResponseEntityExceptionHandl
         final ErrorResponse body = userManagementException.getErrorResponse();
         final HttpStatus httpStatus = userManagementException.getStatusCode();
         return new ResponseEntity(body, httpStatus);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return new ResponseEntity(errors, status);
     }
 
 }
